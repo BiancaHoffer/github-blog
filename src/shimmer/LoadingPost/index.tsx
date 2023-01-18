173795c71a8353df380d5a2264d8b4ec9fc20489
Bianca Hoffer <bianca.mhm1@gmail.com>
2023-01-18 15:57:41 +0000

    Teste shimmer

---
+++ src/shimmer/LoadingPost/index.tsx
@@ -0,0 +1,5 @@
+export function LoadindPost() {
+    return (
+        <h1>LoadindHeaderPost</h1>
+    )
+}
---
+++ src/shimmer/LoadingPostCard/index.tsx
@@ -0,0 +1,5 @@
+export function LoadindCardPost() {
+    return (
+        <h1>LoadindHeaderPost</h1>
+    )
+}
